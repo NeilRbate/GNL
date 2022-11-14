a96c1f4f775baf16156f44d2868c3a201a62a719
Julien Barbate <jbarbate@c1r5p1.42nice.fr>
2022-11-14 11:48:33 +0100

    First commit, files add

---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbarbate <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 07:21:22 by jbarbate          #+#    #+#             */
+/*   Updated: 2022/11/14 09:55:05 by jbarbate         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_free(char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbarbate <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 07:21:05 by jbarbate          #+#    #+#             */
+/*   Updated: 2022/11/14 11:02:28 by jbarbate         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (dstsize > 0)
+	{
+		while (src[i])
+		{
+			if (dstsize > 0 && i < (dstsize - 1))
+			{
+				dst[j] = src[j];
+				j++;
+			}
+			i++;
+		}
+		dst[j] = '\0';
+	}
+	return (ft_strlen(src));
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*ret;
+
+	ret = malloc(sizeof(char) * ft_strlen(s1) + 1);
+	if (!ret)
+		return (0);
+	ft_strlcpy(ret, s1, ft_strlen(s1) + 1);
+	return (ret);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	int		len_t;
+	int		i;
+	int		j;
+	char	*ret;
+
+	i = 0;
+	j = 0;
+	if (s1 == 0 || s2 == 0)
+		return (0);
+	len_t = ft_strlen(s1) + ft_strlen(s2);
+	ret = malloc(sizeof(char) * len_t + 1);
+	if (ret == 0)
+		return (0);
+	while (s1[i])
+	{
+		ret[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+		ret[i++] = s2[j++];
+	ret[i] = '\0';
+	free((char *)s1);
+	return (ret);
+}
